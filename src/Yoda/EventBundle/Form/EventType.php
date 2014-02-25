7eb411d862d3497c6b9e4cafb5e44a14b01fff78
Raphaël Dubigny <rdubigny@gmail.com>
2014-02-25 11:00:52 +0100

    just before adding fixtures

---
+++ src/Yoda/EventBundle/Form/EventType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Yoda\EventBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('imageName')
+            ->add('time')
+            ->add('location')
+            ->add('details')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Yoda\EventBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'yoda_eventbundle_event';
+    }
+}
